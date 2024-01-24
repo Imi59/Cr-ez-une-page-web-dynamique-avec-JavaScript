9b460e358592959128f80685ed60ef3d85aeeb65
Imane Baqua <imane.baqua@hotmail.fr>
2024-01-24 19:37:32 +0100

    création fonction pour récupérer les travaux de l API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,14 @@
-fetch("http://localhost:5678/api/works")
+/*fetch("http://localhost:5678/api/works")
 .then(response => response.json())
 .then(response => console.log(response))
-.catch(error => alert("Erreur : " + error));
+.catch(error => alert("Erreur : " + error));*/
+
+/* JE CREER UNE FONCTION POUR RECUPERER TOUS LES TRAVAUX*/
+async function getworks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    return await response.json();
+    console.log(responseJson);  
+}
+getworks();
+
+/*je veux que les works s'affichent dans le dom*/
