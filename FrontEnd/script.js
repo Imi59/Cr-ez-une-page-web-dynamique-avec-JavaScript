50b2a57fa5b049579471ab3a50c29ff4e3deb1d6
Imane Baqua <imane.baqua@hotmail.fr>
2024-01-25 20:02:19 +0100

    finalisation étape 1.1 : Récupérer les travaux depuis le back end et les afficher OK

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,35 +4,30 @@
 .catch(error => alert("Erreur : " + error));*/
 
 
-/** VARIABLES **/
+/** VARIABLES GLOBALES PROJET 6**/
 const sophieGallery = document.querySelector("#portfolio .gallery");
-console.log(sophieGallery);
 
 
-/* JE CREER UNE FONCTION POUR RECUPERER TOUS LES TRAVAUX*/
+/* JE CREATION FONCTION POUR RECUPERER TOUS LES TRAVAUX DE SWAGGER*/
+
 async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
 }
 getWorks();
 
-async function affichageWorks() {
-    const arrayWorks = await getWorks();
-
-arrayWorks.forEach(element => {
+async function displayWorks() {
+    const allWorks = await getWorks();
+allWorks.forEach(element => {
     const figure = document.createElement("figure");
-    console.log(figure);
     const img = document.createElement("img");
-    console.log(img);
     const figcaption = document.createElement("figcaption");
-    console.log(figcaption);
     img.src = element.imageUrl;
     figcaption.textContent = element.title;
     figure.appendChild(img);
     figure.appendChild(figcaption);
     sophieGallery.appendChild(figure);   
 });
-  
 }
-affichageWorks();
+displayWorks();
 
