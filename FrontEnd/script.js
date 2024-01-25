ba372fd901b574271d13d63ff538c4d7c592590c
Imane Baqua <imane.baqua@hotmail.fr>
2024-01-25 11:39:18 +0100

    creation fonction pour afficher les images récupérées sur l'API

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,12 +3,36 @@
 .then(response => console.log(response))
 .catch(error => alert("Erreur : " + error));*/
 
+
+/** VARIABLES **/
+const sophieGallery = document.querySelector("#portfolio .gallery");
+console.log(sophieGallery);
+
+
 /* JE CREER UNE FONCTION POUR RECUPERER TOUS LES TRAVAUX*/
-async function getworks() {
+async function getWorks() {
     const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
-    console.log(responseJson);  
 }
-getworks();
+getWorks();
 
-/*je veux que les works s'affichent dans le dom*/
+async function affichageWorks() {
+    const arrayWorks = await getWorks();
+
+arrayWorks.forEach(element => {
+    const figure = document.createElement("figure");
+    console.log(figure);
+    const img = document.createElement("img");
+    console.log(img);
+    const figcaption = document.createElement("figcaption");
+    console.log(figcaption);
+    img.src = element.imageUrl;
+    figcaption.textContent = element.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    sophieGallery.appendChild(figure);   
+});
+  
+}
+affichageWorks();
+
