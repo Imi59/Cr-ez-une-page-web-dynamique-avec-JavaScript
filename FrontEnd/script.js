c384881544218293a38328fb99598f58cf692054
Imane Baqua <imane.baqua@hotmail.fr>
2024-01-27 20:17:42 +0100

    ajout add event listener au click des bouttons pour trier etape 1 : vider la gallery au click OK

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -60,8 +60,20 @@ displayBtnCat();
 /*** filtrer au click */
 
 async function filtersClick() {
+    const works = await getWorks(); /**j'utilise encore la fonction qui récupére tous les travaux*/
+/**** je récupére tous mes bouttons */
+const btnsFilters = document.querySelectorAll(".filters_btn");
+btnsFilters.forEach(element => { /**** pour chaque éléments de mes btnsFilters j'ajoute un événement au click */
+    element.addEventListener("click", (evenement) => {
+        /*ici je veux récupérer les id de mes btns */
+        const btnId = evenement.target.id; /**target veut dire qu on demande à afficher les id de chaque événement au click */
+        /*** au click sur un boutton je veux dans un premier temps que ma gallery
+         se vide donc  */ sophieGallery.innerHTML = "";
+         
 
-
+    });
+   
+});
     
 }
 filtersClick();
