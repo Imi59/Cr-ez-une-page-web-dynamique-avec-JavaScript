fdca60d87f20c4a8f09a1216585e87d18f4171e0
Imane Baqua <imane.baqua@hotmail.fr>
2024-02-07 14:07:44 +0100

    nettoyage du code LOGIN.JS ok

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -12,28 +12,27 @@ loginForm.addEventListener("submit", function (e) {
   const password = document.getElementById("password").value;
   const dataLog = {email: email,password: password,};
 
-  // Effectue une demande POST à l'API
+  
   
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {"Content-Type": "application/json", },
-    body: JSON.stringify(dataLog), // Convertit les données en format JSON et les envoie à l'API
+    body: JSON.stringify(dataLog), // Convertion des données en JSON et envoi à l'API
   })
+   //test de la connexion avec l'API
     .then((response) => {
-      //test la connexion avec l'API
       if (!response.ok) {
-        throw new Error("Échec de la connexion"); // En cas d'échec de la réponse de l'API
+        throw new Error("Échec de la connexion"); // Si échec de la réponse de l'API
       }
-      return response.json(); // Convertit la réponse JSON de l'API en objet JavaScript
+      return response.json(); // Convertion de la réponse API en JSON
     })
     .then((data) => {
-      localStorage.setItem("loginToken", data.token); // Stock le token dans le localstorage
-      alert("Connexion réussie"); // Affiche une alerte indiquant que la connexion a réussi
-
-      window.location.href = "index.html"; // Redirige l'utilisateur vers 'index.html' Si la connexion est réussie
+      localStorage.setItem("loginToken", data.token); // Stockage du token dans le localstorage
+      alert("Connexion réussie"); // Alerte qui indique que la connexion a réussi
+      window.location.href = "index.html"; // Utilisateur est redirigé vers 'index.html' en cas de réussite
     })
     .catch((error) => {
-      alert("Erreur dans l’identifiant ou le mot de passe"); // Affiche une alerte en cas d'échec
+      alert("L’identifiant ou le mot de passe est incorrect"); // Alerte en cas d'échec
     });
 });
 
