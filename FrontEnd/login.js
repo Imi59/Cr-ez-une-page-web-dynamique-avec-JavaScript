5c55a21e6e4e1d40f81596d1beaf481d6e041402
Imane Baqua <imane.baqua@hotmail.fr>
2024-02-07 13:55:53 +0100

    modification sur la page d'authentification LOGIN.JS pour rendre le code plus clair et lisible

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,29 +1,23 @@
-// Récupère l'élément du formulaire de connexion avec l'ID 'login-form'
+
 const loginForm = document.getElementById("login-form");
-const loginToken = localStorage.getItem("loginToken"); //l 'endroit ou est stocké le Token dans le localstorage
 const loginbouton = document.getElementById("loginbutton");
+const loginToken = localStorage.getItem("loginToken");
 
 
-// Ajoute un écouteur d'événements pour le formulaire de connexion lorsque l'utilisateur soumet le formulaire
+
+
 loginForm.addEventListener("submit", function (e) {
-  e.preventDefault(); // Empêche le comportement par défaut de soumettre le formulaire
-
-  // Récupère la valeur de l'élément d'entrée 'email' et password du formulaire
+  e.preventDefault();
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-
-  // Préparez les données à envoyer à l'API
-  const loginData = {
-    email: email,
-    password: password,
-  };
+  const dataLog = {email: email,password: password,};
 
   // Effectue une demande POST à l'API
   
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {"Content-Type": "application/json", },
-    body: JSON.stringify(loginData), // Convertit les données en format JSON et les envoie à l'API
+    body: JSON.stringify(dataLog), // Convertit les données en format JSON et les envoie à l'API
   })
     .then((response) => {
       //test la connexion avec l'API
